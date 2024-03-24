d5c9d56a20157edcdfd8944e082980f5b149a10e
Tiddo Loos <tiddo.loos@woonstadrotterdam.nl>
2024-03-24 14:11:31 +0100

    config loader simplificatie

--- woningwaardering/stelsels/config/config.py
+++ woningwaardering/stelsels/config/config.py
@@ -1,9 +1,10 @@
 import yaml
 from typing import Dict, List, Union
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 
 
-class VersieConfig(BaseModel):
+class StelselgroepVersieConfig(BaseModel):
+    versie: str
     begindatum: Union[str, None]
     einddatum: Union[str, None]
 
@@ -12,26 +13,34 @@ class StelselgroepConfig(BaseModel):
     class_naam: str
     begindatum: Union[str, None]
     einddatum: Union[str, None]
-    versies: List[Dict[str, VersieConfig]]
+    versies: List[StelselgroepVersieConfig]
 
 
 class StelselConfig(BaseModel):
+    stelsel: str
     begindatum: Union[str, None]
     einddatum: Union[str, None]
     stelselgroepen: Dict[str, StelselgroepConfig]
 
-
-class Config(BaseModel):
-    stelsel: Dict[str, StelselConfig]
-
     @classmethod
-    def load(cls, stelsel: str = "zelfstandig") -> "Config":
-        path = f"woningwaardering/stelsels/config/{stelsel}.yml"
-        with open(path, "r") as file:
-            config = yaml.safe_load(file)
-        return cls(stelsel=config)
+    def load(cls, stelsel: str = "zelfstandig") -> "StelselConfig":
+        try:
+            path = f"woningwaardering/stelsels/config/{stelsel}.yml"
+            with open(path, "r") as file:
+                config = yaml.safe_load(file)
+            stelsel_config = cls(**config)
+            return stelsel_config
+        except ValidationError as e:
+            print(e, "Geen valide stelsel configuratie.")
+            raise
+        except FileNotFoundError as e:
+            print(e, f"Config file {path} is niet gevonden.")
+            raise
 
-    # @staticmethod
-    # def load_dict(stelsel: str = "zelfstandig") -> dict:
-    #     config = Config.load(stelsel=stelsel)
-    #     return config.model_dump()
+
+stelsel_config = StelselConfig.load()
+# print(stelsel_config)
+print(type(stelsel_config))
+print(stelsel_config.stelselgroepen)
+for stelselgroep, config in stelsel_config.stelselgroepen.items():
+    print(config.versies)
