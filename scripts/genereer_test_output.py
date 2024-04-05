04de15bba9c9f131dd60c9ba9952fb7defc45a6d
Ben Verhees <ben.verhees@woonstadrotterdam.nl>
2024-04-05 23:22:14 +0200

    Toevoegen script om test output te genereren

---
+++ scripts/genereer_test_output.py
@@ -0,0 +1,51 @@
+from datetime import datetime, date
+from pathlib import Path
+
+from woningwaardering.stelsels.zelfstandige_woonruimten.zelfstandige_woonruimten import (
+    ZelfstandigeWoonruimten,
+)
+from woningwaardering.vera.bvg.generated import (
+    EenhedenEenheid,
+)
+
+jaar = datetime.now().year
+PEILDATUM = date(year=jaar, month=1, day=1)
+
+INPUT_DIR = Path("data_modellen/input")
+OUTPUT_DIR = Path("data_modellen/output")
+
+input_file_paths = (INPUT_DIR / "zelfstandige_woonruimten").rglob("*.json")
+
+
+output_file_paths = list((OUTPUT_DIR / "zelfstandige_woonruimten").rglob("*.json"))
+
+for input_file_path in input_file_paths:
+    if input_file_path.name not in [x.name for x in output_file_paths]:
+        output_file_path = (
+            OUTPUT_DIR
+            / "zelfstandige_woonruimten/peildatum"
+            / datetime.strftime(PEILDATUM, "%Y-%m-%d")
+            / input_file_path.name
+        )
+        unverified_path = output_file_path.parent / (
+            output_file_path.stem + ".unverified" + output_file_path.suffix
+        )
+
+        output_file_path.parent.mkdir(parents=True, exist_ok=True)
+
+        # get input model
+        with open(input_file_path, "r+") as f:
+            eenheid_input = EenhedenEenheid.model_validate_json(f.read())
+
+            zelfstandige_woonruimten = ZelfstandigeWoonruimten(peildatum=PEILDATUM)
+            woningwaardering_resultaat = zelfstandige_woonruimten.bereken(eenheid_input)
+            # write output model
+            with open(
+                unverified_path,
+                "w+",
+            ) as f:
+                f.write(
+                    woningwaardering_resultaat.model_dump_json(
+                        by_alias=True, indent=2, exclude_none=True
+                    )
+                )
