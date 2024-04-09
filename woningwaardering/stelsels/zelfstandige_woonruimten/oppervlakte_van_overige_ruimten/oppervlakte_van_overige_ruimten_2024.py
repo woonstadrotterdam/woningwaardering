b5a2e27885b6ffdd278bec19cad2e876e30d354c
Ben Verhees <ben.verhees@woonstadrotterdam.nl>
2024-04-09 11:15:16 +0200

    Voeg het aantal gewaardeerde kasten toe aan de criteriumnaam voor overige ruimten

--- woningwaardering/stelsels/zelfstandige_woonruimten/oppervlakte_van_overige_ruimten/oppervlakte_van_overige_ruimten_2024.py
+++ woningwaardering/stelsels/zelfstandige_woonruimten/oppervlakte_van_overige_ruimten/oppervlakte_van_overige_ruimten_2024.py
@@ -78,6 +78,8 @@ class OppervlakteVanOverigeRuimten2024(Stelselgroepversie):
                     )
                     continue
 
+                criterium_naam = ruimte.naam
+
                 # Van vaste kasten (kleiner dan 2m²) wordt de netto oppervlakte bepaald
                 # en bij de oppervlakte van het betreffende vertrek opgeteld.
                 # Een kast, (kleiner dan 2m²) waarvan de deur uitkomt op een
@@ -98,22 +100,27 @@ class OppervlakteVanOverigeRuimten2024(Stelselgroepversie):
                         and verbonden_ruimte.oppervlakte < Decimal("2")
                     ]
 
-                    ruimte.oppervlakte += sum(
-                        [
-                            verbonden_kast.oppervlakte
-                            for verbonden_kast in verbonden_kasten
-                            if verbonden_kast.oppervlakte is not None
-                        ]
-                    )
+                    aantal_verbonden_kasten = len(verbonden_kasten)
 
-                    if ruimte.inhoud is not None:
-                        ruimte.inhoud += sum(
+                    if aantal_verbonden_kasten > 0:
+                        ruimte.oppervlakte += sum(
                             [
-                                verbonden_kast.inhoud
+                                verbonden_kast.oppervlakte
                                 for verbonden_kast in verbonden_kasten
-                                if verbonden_kast.inhoud is not None
+                                if verbonden_kast.oppervlakte is not None
                             ]
                         )
+
+                        if ruimte.inhoud is not None:
+                            ruimte.inhoud += sum(
+                                [
+                                    verbonden_kast.inhoud
+                                    for verbonden_kast in verbonden_kasten
+                                    if verbonden_kast.inhoud is not None
+                                ]
+                            )
+
+                        criterium_naam = f"{ruimte.naam} + {aantal_verbonden_kasten} {aantal_verbonden_kasten == 1 and 'kast' or 'kasten'}"
 
                 if ruimte.oppervlakte is not None and ruimte.oppervlakte < 2:
                     logger.debug(
@@ -126,7 +133,7 @@ class OppervlakteVanOverigeRuimten2024(Stelselgroepversie):
                 woningwaardering.criterium = (
                     WoningwaarderingResultatenWoningwaarderingCriterium(
                         meeteenheid=Meeteenheid.vierkante_meter_m2.value,
-                        naam=ruimte.naam,
+                        naam=criterium_naam,
                     )
                 )
 
