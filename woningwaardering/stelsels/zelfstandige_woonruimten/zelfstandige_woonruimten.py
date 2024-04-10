de491a375c80495ce01870487e64f534ba28e471
Ben Verhees <ben.verhees@woonstadrotterdam.nl>
2024-04-10 17:04:10 +0200

    Leesbare tabel toevoegen aan output

--- woningwaardering/stelsels/zelfstandige_woonruimten/zelfstandige_woonruimten.py
+++ woningwaardering/stelsels/zelfstandige_woonruimten/zelfstandige_woonruimten.py
@@ -1,5 +1,7 @@
 from datetime import date
 
+from prettytable import PrettyTable
+
 from woningwaardering.stelsels.stelsel import Stelsel
 from woningwaardering.vera.bvg.generated import (
     EenhedenEenheid,
@@ -24,6 +26,56 @@ if __name__ == "__main__":
         "r+",
     )
     eenheid = EenhedenEenheid.model_validate_json(f.read())
+    woningwaardering_resultaat = zel.bereken(eenheid)
     print(
-        zel.bereken(eenheid).model_dump_json(by_alias=True, indent=2, exclude_none=True)
+        woningwaardering_resultaat.model_dump_json(
+            by_alias=True, indent=2, exclude_none=True
+        )
     )
+
+    table = PrettyTable()
+    table.field_names = ["Groep", "Naam", "Aantal", "Meeteenheid", "Punten"]
+    table.align = "l"
+    table.align["Aantal"] = "r"
+    table.align["Punten"] = "r"
+
+    for woningwaardering_groep in woningwaardering_resultaat.groepen or []:
+        for woningwaardering in woningwaardering_groep.woningwaarderingen or []:
+            table.add_row(
+                [
+                    woningwaardering_groep.criterium_groep.stelselgroep.naam,
+                    woningwaardering.criterium.naam,
+                    woningwaardering.aantal,
+                    woningwaardering.criterium.meeteenheid.naam,
+                    woningwaardering.punten or "",
+                ]
+            )
+        table.add_row(
+            [
+                woningwaardering_groep.criterium_groep.stelselgroep.naam,
+                "Subtotaal",
+                sum(
+                    [
+                        woningwaardering.aantal
+                        for woningwaardering in woningwaardering_groep.woningwaarderingen
+                        or []
+                        if woningwaardering.aantal is not None
+                    ]
+                ),
+                "",
+                woningwaardering_groep.punten,
+            ],
+            divider=True,
+        )
+    table.add_row(
+        [
+            woningwaardering_resultaat.stelsel.naam,
+            "Totaal",
+            "",
+            "",
+            woningwaardering_resultaat.punten,
+        ],
+        divider=True,
+    )
+
+    print(table)
