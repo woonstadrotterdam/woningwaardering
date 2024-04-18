2c2b36a878bb219c9372e1298180a48dfe7f3505
Tiddo Loos <tiddo.loos@woonstadrotterdam.nl>
2024-04-18 16:25:34 +0200

    error raisen ipv een error loggen voor None values

--- woningwaardering/stelsels/zelfstandige_woonruimten/oppervlakte_van_overige_ruimten/oppervlakte_van_overige_ruimten_2024.py
+++ woningwaardering/stelsels/zelfstandige_woonruimten/oppervlakte_van_overige_ruimten/oppervlakte_van_overige_ruimten_2024.py
@@ -113,15 +113,19 @@ class OppervlakteVanOverigeRuimten2024(Stelselgroepversie):
         woningwaardering_groep.woningwaarderingen = []
 
         for ruimte in eenheid.ruimten or []:
+            logger.debug(f"Processsing ruimte: {ruimte.id}")
             if ruimte.oppervlakte is None:
-                logger.warning(f"Ruimte {ruimte.id} heeft geen oppervlakte")
-                continue
+                error_msg = f"Ruimte {ruimte.id} heeft geen oppervlakte"
+                logger.error(error_msg)
+                raise TypeError(error_msg)
             if ruimte.detail_soort is None:
-                logger.warning(f"Ruimte {ruimte.id} heeft geen detailsoort")
-                continue
+                error_msg = f"Ruimte {ruimte.id} heeft geen detailsoort"
+                logger.error(error_msg)
+                raise TypeError(error_msg)
             if ruimte.detail_soort.code is None:
-                logger.warning(f"Ruimte {ruimte.id} heeft geen detailsoortcode")
-                continue
+                error_msg = f"Ruimte {ruimte.id} heeft geen detailsoortcode"
+                logger.error(error_msg)
+                raise TypeError(error_msg)
 
             if ruimte.soort is not None and (
                 ruimte.soort.code == Ruimtesoort.overige_ruimtes.code
