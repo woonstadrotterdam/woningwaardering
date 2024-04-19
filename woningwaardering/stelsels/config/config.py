4e8c7d02b1fc1c6b425ff3b379817fd84ab871e3
Ben Verhees <ben.verhees@woonstadrotterdam.nl>
2024-04-19 11:41:55 +0200

    Default begin- en einddatum in class zodat deze niet in config gespecificeerd hoeven te worden

--- woningwaardering/stelsels/config/config.py
+++ woningwaardering/stelsels/config/config.py
@@ -12,21 +12,21 @@ from woningwaardering.vera.referentiedata import (
 
 class Stelselgroepversieconfig(BaseModel):
     class_naam: str
-    begindatum: date
-    einddatum: date
+    begindatum: date = date.min
+    einddatum: date = date.max
 
 
 class Stelselgroepconfig(BaseModel):
     class_naam: str
-    begindatum: date
-    einddatum: date
+    begindatum: date = date.min
+    einddatum: date = date.max
     versies: List[Stelselgroepversieconfig]
 
 
 class Stelselconfig(BaseModel):
     stelsel: str
-    begindatum: date
-    einddatum: date
+    begindatum: date = date.min
+    einddatum: date = date.max
     stelselgroepen: Dict[str, Stelselgroepconfig]
 
     @classmethod
