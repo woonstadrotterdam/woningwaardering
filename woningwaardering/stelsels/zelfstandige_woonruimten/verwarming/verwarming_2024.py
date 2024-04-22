7f607c37f265ef8d0501397cd30475270f4bd2d0
Tiddo Loos <tiddo.loos@woonstadrotterdam.nl>
2024-04-22 14:50:55 +0200

    implementatie punten en max punten voor verwarming

--- woningwaardering/stelsels/zelfstandige_woonruimten/verwarming/verwarming_2024.py
+++ woningwaardering/stelsels/zelfstandige_woonruimten/verwarming/verwarming_2024.py
@@ -1,9 +1,13 @@
-from decimal import ROUND_HALF_UP, Decimal
+from decimal import Decimal
 
 from loguru import logger
 
 
+from woningwaardering.stelsels import utils
 from woningwaardering.stelsels.stelselgroepversie import Stelselgroepversie
+from woningwaardering.stelsels.zelfstandige_woonruimten.utils import (
+    vertrek_telt_als_vertrek,
+)
 from woningwaardering.vera.bvg.generated import (
     EenhedenEenheid,
     Referentiedata,
@@ -48,13 +52,85 @@ class Verwarming2024(Stelselgroepversie):
         )
 
         woningwaardering_groep.woningwaarderingen = []
-        woningwaardering_groep.woningwaarderingen.append(
-            WoningwaarderingResultatenWoningwaardering(
-                criterium=WoningwaarderingResultatenWoningwaarderingCriterium(
-                    naam="NotImplemented"
+        totaal_punten_overige_ruimten = Decimal("0")
+
+        for ruimte in eenheid.ruimten or []:
+            # TODO: checks of ruimte in aanmerking komt voor Verwarming
+            if ruimte.soort is None:
+                logger.error(f"Ruimte {ruimte.id} heeft geen soort.")
+            if ruimte.soort is not None and ruimte.soort.code is None:
+                logger.error(f"Ruimtesoort {ruimte.id} heeft geen code.")
+            if (
+                ruimte.soort is not None
+                and ruimte.soort.code is not None
+                and ruimte.soort.code
+                not in [
+                    Ruimtesoort.vertrek.code,
+                    Ruimtesoort.overige_ruimtes.code,
+                ]
+            ):
+                logger.debug(
+                    f"Ruimte {ruimte.id} komt niet in aanmerking voor punten voor Verwarming."
+                )
+                continue
+
+            woningwaardering = WoningwaarderingResultatenWoningwaardering()
+
+            woningwaardering.criterium = (
+                WoningwaarderingResultatenWoningwaarderingCriterium(
+                    naam=ruimte.naam,
                 )
             )
-        )
+
+            punten = Decimal(str(punten_per_ruimte[Ruimtesoort.vertrek.code]))
+
+            if not vertrek_telt_als_vertrek(ruimte):
+                punten = Decimal(
+                    str(punten_per_ruimte[Ruimtesoort.overige_ruimtes.code])
+                )
+
+                if totaal_punten_overige_ruimten >= Decimal("4.0"):
+                    logger.debug(
+                        f"De overige ruimten hebben bij elkaar {totaal_punten_overige_ruimten} punten behaald: {ruimte.naam} wordt niet meegeteld voor Verwarming."
+                    )
+                    continue
+
+                # Kijk of de punten de maximum van 4.0 overschrijden
+                nieuw_totaal_punten_overige_ruimten = (
+                    totaal_punten_overige_ruimten + punten
+                )
+                # Als de punten de maximum van 4.0 overschrijden, dan wordt het aantal punten dat nog mag worden gegeven voor de ruimte aangepast
+                if nieuw_totaal_punten_overige_ruimten >= Decimal("4.0"):
+                    logger.debug(
+                        f"De maximum punten voor {Ruimtesoort.overige_ruimtes.naam} zijn behaald: punten voor {ruimte.naam} worden gecorrigeerd."
+                    )
+                    punten = Decimal(
+                        str(
+                            min(
+                                Decimal("4.0") - totaal_punten_overige_ruimten,
+                                punten,
+                            )
+                        )
+                    )
+
+                totaal_punten_overige_ruimten += punten
+                logger.debug(
+                    f"Ruimte {ruimte.naam} telt als verwarmde {Ruimtesoort.overige_ruimtes.naam} en krijgt {punten} punten."
+                )
+
+            else:
+                logger.debug(
+                    f"Ruimte {ruimte.naam} telt als verwarmd {Ruimtesoort.vertrek.naam} en krijgt {punten} punten."
+                )
+
+            woningwaardering_groep.woningwaarderingen.append(
+                WoningwaarderingResultatenWoningwaardering(
+                    criterium=WoningwaarderingResultatenWoningwaarderingCriterium(
+                        naam=ruimte.naam,
+                    ),
+                    aantal=punten,
+                )
+            )
 
         punten = Decimal(
             sum(
@@ -62,7 +138,7 @@ class Verwarming2024(Stelselgroepversie):
                 for woningwaardering in woningwaardering_groep.woningwaarderingen or []
                 if woningwaardering.aantal is not None
             )
-        ).quantize(Decimal("1"), ROUND_HALF_UP) * Decimal("1")
+        )
 
         woningwaardering_groep.punten = float(punten)
         return woningwaardering_groep
@@ -89,3 +165,23 @@ class Verwarming2024(Stelselgroepversie):
             else Eenheidklimaatbeheersingsoort.individueel.code
         ]
         return punten_per_ruimte
+
+
+if __name__ == "__main__":
+    file = open(
+        "tests/data/input/zelfstandige_woonruimten/12006000004.json",
+        "r+",
+    )
+    eenheid = EenhedenEenheid.model_validate_json(file.read())
+
+    woningwaardering_resultaat = Verwarming2024.bereken(eenheid)
+
+    print(
+        woningwaardering_resultaat.model_dump_json(
+            by_alias=True, indent=2, exclude_none=True
+        )
+    )
+
+    tabel = utils.naar_tabel(woningwaardering_resultaat)
+
+    print(tabel)
