229de3e2c9831496ff4430ba6681d828708de549
Tiddo Loos <tiddo.loos@woonstadrotterdam.nl>
2024-04-30 13:18:10 +0200

    stelselgroep energieprestatie opzet

--- woningwaardering/stelsels/zelfstandige_woonruimten/__init__.py
+++ woningwaardering/stelsels/zelfstandige_woonruimten/__init__.py
@@ -1,10 +1,12 @@
 from .oppervlakte_van_vertrekken import OppervlakteVanVertrekken
 from .oppervlakte_van_overige_ruimten import OppervlakteVanOverigeRuimten
 from .verwarming import Verwarming
+from .energieprestatie import Energieprestatie
 
 
 __all__ = [
     "OppervlakteVanVertrekken",
     "OppervlakteVanOverigeRuimten",
     "Verwarming",
+    "Energieprestatie",
 ]
---
+++ woningwaardering/stelsels/zelfstandige_woonruimten/energieprestatie/__init__.py
@@ -0,0 +1,8 @@
+from .energieprestatie import Energieprestatie
+from .energieprestatie_2024 import Energieprestatie2024
+
+
+__all__ = [
+    "Energieprestatie",
+    "Energieprestatie2024",
+]
