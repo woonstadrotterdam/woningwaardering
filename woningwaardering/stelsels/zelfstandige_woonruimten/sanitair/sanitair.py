b7050379fdf2cfaf18a0fb4dbdc2598bc20cd368
Tomer Gabay <tomergabay001@gmail.com>
2024-05-17 09:11:39 +0200

    opzet sanitair gegenereerd

--- woningwaardering/stelsels/zelfstandige_woonruimten/sanitair/sanitair.py
+++ woningwaardering/stelsels/zelfstandige_woonruimten/sanitair/sanitair.py
@@ -2,34 +2,33 @@ from datetime import date
 
 from loguru import logger
 
-from woningwaardering.stelsels.stelsel import Stelsel
 from woningwaardering.stelsels import utils
+from woningwaardering.stelsels.stelselgroep import Stelselgroep
 from woningwaardering.vera.bvg.generated import (
     EenhedenEenheid,
 )
 from woningwaardering.vera.referentiedata import (
     Woningwaarderingstelsel,
+    Woningwaarderingstelselgroep,
 )
 
 
-class ZelfstandigeWoonruimten(Stelsel):
+class Sanitair(Stelselgroep):
     def __init__(self, peildatum: date = date.today()) -> None:
-        super().__init__(
-            stelsel=Woningwaarderingstelsel.zelfstandige_woonruimten,
-            peildatum=peildatum,
-        )
+        self.stelsel = Woningwaarderingstelsel.zelfstandige_woonruimten
+        self.stelselgroep = Woningwaarderingstelselgroep.sanitair
+        super().__init__(peildatum=peildatum)
 
 
 if __name__ == "__main__":
     logger.enable("woningwaardering")
 
-    zelfstandige_woonruimten = ZelfstandigeWoonruimten()
-    file = open(
-        "./tests/data/input/generiek/37101000032.json",
-        "r+",
-    )
-    eenheid = EenhedenEenheid.model_validate_json(file.read())
-    woningwaardering_resultaat = zelfstandige_woonruimten.bereken(eenheid)
+    sanitair = Sanitair()
+    with open("./tests/data/input/generiek/37101000032.json", "r+") as file:
+        eenheid = EenhedenEenheid.model_validate_json(file.read())
+
+    woningwaardering_resultaat = sanitair.bereken(eenheid)
+
     print(
         woningwaardering_resultaat.model_dump_json(
             by_alias=True, indent=2, exclude_none=True
