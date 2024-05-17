11626a762d57829583b3d225bac788008ae7a862
Tomer Gabay <tomergabay001@gmail.com>
2024-05-17 12:42:15 +0200

    update van referentiedata vanuit vera

--- woningwaardering/stelsels/zelfstandige_woonruimten/sanitair/sanitair_2024.py
+++ woningwaardering/stelsels/zelfstandige_woonruimten/sanitair/sanitair_2024.py
@@ -40,26 +40,36 @@ class Sanitair2024(Stelselgroepversie):
             if heeft_bouwkundig_element(
                 ruimte, Bouwkundigelementdetailsoort.closetcombinatie.code
             ):
-                totals["closetcombinatie"]["punten"] += Decimal("3")
-                totals["closetcombinatie"]["aantal"] += 1
+                totals[Bouwkundigelementdetailsoort.closetcombinatie.naam][
+                    "punten"
+                ] += Decimal("3")
+                totals[Bouwkundigelementdetailsoort.closetcombinatie.naam][
+                    "aantal"
+                ] += 1
 
             if heeft_bouwkundig_element(
                 ruimte, Bouwkundigelementdetailsoort.wastafel.code
             ):
-                totals["wastafel"]["punten"] += Decimal("1")
-                totals["wastafel"]["aantal"] += 1
+                totals[Bouwkundigelementdetailsoort.wastafel.naam]["punten"] += Decimal(
+                    "1"
+                )
+                totals[Bouwkundigelementdetailsoort.wastafel.naam]["aantal"] += 1
 
             if heeft_bouwkundig_element(
                 ruimte, Bouwkundigelementdetailsoort.bidet.code
             ):
-                totals["bidet"]["punten"] += Decimal("1")
-                totals["bidet"]["aantal"] += 1
+                totals[Bouwkundigelementdetailsoort.bidet.naam]["punten"] += Decimal(
+                    "1"
+                )
+                totals[Bouwkundigelementdetailsoort.bidet.naam]["aantal"] += 1
 
             if heeft_bouwkundig_element(
                 ruimte, Bouwkundigelementdetailsoort.lavet.code
             ):
-                totals["lavet"]["punten"] += Decimal("1")
-                totals["lavet"]["aantal"] += 1
+                totals[Bouwkundigelementdetailsoort.lavet.naam]["punten"] += Decimal(
+                    "1"
+                )
+                totals[Bouwkundigelementdetailsoort.lavet.naam]["aantal"] += 1
 
             # code voor bad en douche, of bad-douche in zelfde ruimte
             bad_aanwezig = heeft_bouwkundig_element(
@@ -69,26 +79,31 @@ class Sanitair2024(Stelselgroepversie):
                 ruimte, Bouwkundigelementdetailsoort.douche.code
             )
             if bad_aanwezig and douche_aanwezig:
-                totals["bad en douche in zelfde ruimte"]["punten"] += Decimal("7")
-                totals["bad en douche in zelfde ruimte"]["aantal"] += 1
+                totals[
+                    f"{Bouwkundigelementdetailsoort.bad.naam} en {Bouwkundigelementdetailsoort.douche.naam} in zelfde ruimte"
+                ]["punten"] += Decimal("7")
+                totals[
+                    f"{Bouwkundigelementdetailsoort.bad.naam} en {Bouwkundigelementdetailsoort.douche.naam} in zelfde ruimte"
+                ]["aantal"] += 1
 
             elif bad_aanwezig:
-                totals["bad"]["punten"] += Decimal("6")
-                totals["bad"]["aantal"] += 1
+                totals[Bouwkundigelementdetailsoort.bad.naam]["punten"] += Decimal("6")
+                totals[Bouwkundigelementdetailsoort.bad.naam]["aantal"] += 1
 
             elif douche_aanwezig:
-                totals["douche"]["punten"] += Decimal("4")
-                totals["douche"]["aantal"] += 1
+                totals[Bouwkundigelementdetailsoort.douche.naam]["punten"] += Decimal(
+                    "4"
+                )
+                totals[Bouwkundigelementdetailsoort.douche.naam]["aantal"] += 1
 
-        for element, values in totals.items():
-            naam = f"{element.capitalize()}"
+        for naam, waarden in totals.items():
             woningwaardering_groep.woningwaarderingen.append(
                 WoningwaarderingResultatenWoningwaardering(
                     criterium=WoningwaarderingResultatenWoningwaarderingCriterium(
                         naam=naam
                     ),
-                    punten=values["punten"],
-                    aantal=values["aantal"],
+                    punten=waarden["punten"],
+                    aantal=waarden["aantal"],
                 )
             )
 
