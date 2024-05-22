a932bb73f32e9c4a26aa69697f24dbbced6ee14e
Tomer Gabay <tomergabay001@gmail.com>
2024-05-22 17:42:08 +0200

    fix import error in pre-push stage van ci pipeline in Github

--- woningwaardering/stelsels/zelfstandige_woonruimten/__init__.py
+++ woningwaardering/stelsels/zelfstandige_woonruimten/__init__.py
@@ -3,8 +3,10 @@ from .oppervlakte_van_overige_ruimten import OppervlakteVanOverigeRuimten
 from .oppervlakte_van_vertrekken import OppervlakteVanVertrekken
 from .sanitair import Sanitair
 from .verwarming import Verwarming
+from .zelfstandige_woonruimten import ZelfstandigeWoonruimten
 
 __all__ = [
+    "ZelfstandigeWoonruimten",
     "OppervlakteVanVertrekken",
     "OppervlakteVanOverigeRuimten",
     "Verwarming",
