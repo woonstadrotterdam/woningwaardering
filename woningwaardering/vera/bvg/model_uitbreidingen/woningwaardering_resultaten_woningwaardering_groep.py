5b28d8f7700c52c0be92e0987d1942247cf59cf0
Ben Verhees <ben.verhees@woonstadrotterdam.nl>
2024-06-24 14:23:26 +0200

    Toevoegen uitbreidings bestanden

---
+++ woningwaardering/vera/bvg/model_uitbreidingen/woningwaardering_resultaten_woningwaardering_groep.py
@@ -0,0 +1,10 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class _WoningwaarderingResultatenWoningwaarderingGroep(BaseModel):
+    # https://github.com/Aedes-datastandaarden/vera-openapi/issues/65
+    opslagpercentage: Optional[float] = None
+    """
+    Het huurprijsopslagpercentage dat is toegekend.
+    """
