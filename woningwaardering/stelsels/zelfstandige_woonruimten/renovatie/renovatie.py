ee1e11c261c7dcab6b43206c7924f31a0f186084
Ben Verhees <ben.verhees@woonstadrotterdam.nl>
2024-06-24 14:31:47 +0200

    Implementatie opzet stelselgroep Renovatie

---
+++ woningwaardering/stelsels/zelfstandige_woonruimten/renovatie/renovatie.py
@@ -0,0 +1,42 @@
+from datetime import date
+
+from loguru import logger
+
+from woningwaardering.stelsels import utils
+from woningwaardering.stelsels.stelselgroep import Stelselgroep
+from woningwaardering.vera.bvg.generated import (
+    EenhedenEenheid,
+)
+from woningwaardering.vera.referentiedata import (
+    Woningwaarderingstelsel,
+    Woningwaarderingstelselgroep,
+)
+
+
+class Renovatie(Stelselgroep):
+    def __init__(self, peildatum: date = date.today()) -> None:
+        self.stelsel = Woningwaarderingstelsel.zelfstandige_woonruimten
+        self.stelselgroep = Woningwaarderingstelselgroep.renovatie
+        super().__init__(peildatum=peildatum)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    logger.enable("woningwaardering")
+
+    renovatie = Renovatie()
+    with open(
+        "tests/data/generiek/input/37101000032.json", "r+"
+    ) as file:
+        eenheid = EenhedenEenheid.model_validate_json(file.read())
+
+        woningwaardering_resultaat = renovatie.bereken(eenheid)
+
+        print(
+            woningwaardering_resultaat.model_dump_json(
+                by_alias=True, indent=2, exclude_none=True
+            )
+        )
+
+        tabel = utils.naar_tabel(woningwaardering_resultaat)
+
+        print(tabel)
