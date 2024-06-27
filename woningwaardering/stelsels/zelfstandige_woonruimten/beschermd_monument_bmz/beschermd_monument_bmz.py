df3afffd86a31667f5c828a1a1c6a6bb5a458188
Ben Verhees <ben.verhees@woonstadrotterdam.nl>
2024-06-27 17:08:20 +0200

    Implementatie Rijksmonumenten met fallback naar api cultureel erfgoed

--- woningwaardering/stelsels/zelfstandige_woonruimten/beschermd_monument_bmz/beschermd_monument_bmz.py
+++ woningwaardering/stelsels/zelfstandige_woonruimten/beschermd_monument_bmz/beschermd_monument_bmz.py
@@ -1,6 +1,8 @@
 from datetime import date
 
 from loguru import logger
+from rdflib import Graph, Literal, Namespace
+from rdflib.plugins.sparql import prepareQuery
 
 from woningwaardering.stelsels import utils
 from woningwaardering.stelsels.stelselgroep import Stelselgroep
@@ -13,11 +15,53 @@ from woningwaardering.vera.referentiedata import (
 )
 
 
+CEO = Namespace("https://linkeddata.cultureelerfgoed.nl/def/ceo#")
+BAG = Namespace("http://bag.basisregistraties.overheid.nl/bag/id/")
+
+endpoint = "https://api.linkeddata.cultureelerfgoed.nl/datasets/rce/cho/sparql"
+
+
+query_template = """
+ASK
+WHERE {{
+    SERVICE <{endpoint}> {{
+        ?monument a ceo:Rijksmonument ;
+            ceo:heeftBasisregistratieRelatie ?basisregistratieRelatie .
+        ?basisregistratieRelatie ceo:heeftBAGRelatie ?bagRelatie .
+        ?bagRelatie ceo:verblijfsobjectIdentificatie ?verblijfsobjectIdentificatie .
+    }}
+}}
+"""
+
+rijksmonumenten_query = prepareQuery(
+    query_template.format(endpoint=endpoint), initNs={"ceo": CEO, "bag": BAG}
+)
+
+
 class BeschermdMonumentBmz(Stelselgroep):
     def __init__(self, peildatum: date = date.today()) -> None:
         self.stelsel = Woningwaarderingstelsel.zelfstandige_woonruimten
         self.stelselgroep = Woningwaarderingstelselgroep.beschermd_monument_bmz
         super().__init__(peildatum=peildatum)
+
+    @staticmethod
+    def is_rijksmonument(verblijfsobjectIdentificatie: str) -> bool:
+        if not str.isnumeric(verblijfsobjectIdentificatie):
+            raise ValueError("VerblijfsobjectIdentificatie moet numeriek zijn")
+
+        graph = Graph()
+
+        result = graph.query(
+            rijksmonumenten_query,
+            initBindings={
+                "verblijfsobjectIdentificatie": Literal(verblijfsobjectIdentificatie),
+            },
+        )
+
+        if result is None or result.askAnswer is None:
+            return False
+        else:
+            return result.askAnswer
 
 
 if __name__ == "__main__":  # pragma: no cover
