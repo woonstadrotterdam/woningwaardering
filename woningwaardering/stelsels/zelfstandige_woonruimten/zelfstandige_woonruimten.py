96192301be629507330f61058978bb1c921c7a25
Tomer Gabay <tomergabay001@gmail.com>
2024-07-11 09:37:27 +0200

    peildatum toegevoegd aan woningwaardering/stelsels/zelfstandige_woonruimten/zelfstandige_woonruimten.py ivm anders errors

--- woningwaardering/stelsels/zelfstandige_woonruimten/zelfstandige_woonruimten.py
+++ woningwaardering/stelsels/zelfstandige_woonruimten/zelfstandige_woonruimten.py
@@ -23,7 +23,7 @@ class ZelfstandigeWoonruimten(Stelsel):
 if __name__ == "__main__":  # pragma: no cover
     logger.enable("woningwaardering")
 
-    zelfstandige_woonruimten = ZelfstandigeWoonruimten()
+    zelfstandige_woonruimten = ZelfstandigeWoonruimten(peildatum=date(2024, 1, 1))
     with open(
         "tests/data/generiek/input/37101000032.json",
         "r+",
