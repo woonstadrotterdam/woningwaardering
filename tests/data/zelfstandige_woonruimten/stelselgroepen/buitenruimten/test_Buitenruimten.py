45c89b1eb625f92bd569f7a8be6c8434f6d109d5
Tomer Gabay <tomergabay001@gmail.com>
2024-09-02 11:25:39 +0200

    specifieke tests van prive-buitenruimten (oude stelselgroep) omgebouwd naar buitenruimten (nieuwe stelselgroep)

--- tests/data/zelfstandige_woonruimten/stelselgroepen/buitenruimten/test_Buitenruimten.py
+++ tests/data/zelfstandige_woonruimten/stelselgroepen/buitenruimten/test_Buitenruimten.py
@@ -4,7 +4,7 @@ import pytest
 
 from tests.test_utils import assert_output_model, laad_specifiek_input_en_output_model
 from woningwaardering.stelsels.zelfstandige_woonruimten import (
-    PriveBuitenruimten,
+    Buitenruimten,
 )
 from woningwaardering.vera.bvg.generated import (
     WoningwaarderingResultatenWoningwaarderingGroep,
@@ -24,43 +24,39 @@ def specifieke_input_en_output_model(request):
     )
 
 
-def test_PriveBuitenruimten(
+def test_Buitenruimten(
     zelfstandige_woonruimten_inputmodel, woningwaardering_resultaat, peildatum
 ):
-    prive_buitenruimten = PriveBuitenruimten(peildatum=peildatum)
-    resultaat = prive_buitenruimten.bereken(
+    buitenruimten = Buitenruimten(peildatum=peildatum)
+    resultaat = buitenruimten.bereken(
         zelfstandige_woonruimten_inputmodel, woningwaardering_resultaat
     )
     assert isinstance(resultaat, WoningwaarderingResultatenWoningwaarderingGroep)
 
 
-def test_PriveBuitenruimten_output(
-    zelfstandige_woonruimten_input_en_outputmodel, peildatum
-):
+def test_Buitenruimten_output(zelfstandige_woonruimten_input_en_outputmodel, peildatum):
     eenheid_input, eenheid_output = zelfstandige_woonruimten_input_en_outputmodel
-    prive_buitenruimten = PriveBuitenruimten(peildatum=peildatum)
+    buitenruimten = Buitenruimten(peildatum=peildatum)
 
     resultaat = WoningwaarderingResultatenWoningwaarderingResultaat()
-    resultaat.groepen = [prive_buitenruimten.bereken(eenheid_input)]
+    resultaat.groepen = [buitenruimten.bereken(eenheid_input)]
 
     assert_output_model(
         resultaat,
         eenheid_output,
-        Woningwaarderingstelselgroep.prive_buitenruimten,
+        Woningwaarderingstelselgroep.buitenruimten,
     )
 
 
-def test_PriveBuitenruimten_specifiek_output(
-    specifieke_input_en_output_model, peildatum
-):
+def test_Buitenruimten_specifiek_output(specifieke_input_en_output_model, peildatum):
     eenheid_input, eenheid_output = specifieke_input_en_output_model
-    prive_buitenruimten = PriveBuitenruimten(peildatum=peildatum)
+    buitenruimten = Buitenruimten(peildatum=peildatum)
 
     resultaat = WoningwaarderingResultatenWoningwaarderingResultaat()
-    resultaat.groepen = [prive_buitenruimten.bereken(eenheid_input)]
+    resultaat.groepen = [buitenruimten.bereken(eenheid_input)]
 
     assert_output_model(
         resultaat,
         eenheid_output,
-        Woningwaarderingstelselgroep.prive_buitenruimten,
+        Woningwaarderingstelselgroep.buitenruimten,
     )
