4d789f8b1178e48a492f6a8a39f1dda2bd2c8405
Tomer Gabay <tomergabay001@gmail.com>
2024-09-23 11:21:20 +0200

    voeg warning toe wanneer de woonplaats van een adres een corop-plaats is

---
+++ woningwaardering/stelsels/zelfstandige_woonruimten/punten_voor_de_woz_waarde/corop_plaatsen.py
@@ -0,0 +1,191 @@
+# https://www.huurcommissie.nl/huurcommissie-helpt/beleidsboeken_html/waarderingsstelsel-zelfstandige-woonruimte/bijlage-3
+COROP_PLAATSEN = (
+    # Woonplaatsen in Aalsmeer
+    "aalsmeer",
+    "kudelstaart",
+    # Woonplaatsen in Amersfoort
+    "amersfoort",
+    "hoogland",
+    "hooglanderveen",
+    # Woonplaatsen in Amstelveen
+    "amstelveen",
+    "bovenkerk",
+    "nes aan de amstel",
+    # Woonplaatsen in Amsterdam
+    "amsterdam",
+    "driemond",
+    "sloten",
+    # Woonplaatsen in Baarn
+    "baarn",
+    "lage vuursche",
+    "eembrugge",
+    # Woonplaatsen in Beemster
+    "middenbeemster",
+    "noordbeemster",
+    "westbeemster",
+    "zuidoostbeemster",
+    # Woonplaatsen in Bunnik
+    "bunnik",
+    "odijk",
+    "werkhoven",
+    # Woonplaatsen in Bunschoten
+    "bunschoten",
+    "spakenburg",
+    # Woonplaatsen in De Bilt
+    "de bilt",
+    "bilthoven",
+    "groenekan",
+    "hollandsche rading",
+    "maartensdijk",
+    "westbroek",
+    # Woonplaatsen in De Ronde Venen
+    "abcoude",
+    "amstelhoek",
+    "baambrugge",
+    "de hoef",
+    "mijdrecht",
+    "vinkeveen",
+    "waverveen",
+    "wilnis",
+    # Woonplaatsen in Diemen
+    "diemen",
+    # Woonplaatsen in Edam-Volendam
+    "edam",
+    "volendam",
+    "schardam",
+    "middelie",
+    "beets",
+    "oosthuizen",
+    "warder",
+    # Woonplaatsen in Eemnes
+    "eemnes",
+    # Woonplaatsen in Graft-De Rijp
+    "graft",
+    "de rijp",
+    "noordeinde",
+    "oost-graftdijk",
+    "west-graftdijk",
+    # Woonplaatsen in Haarlemmermeer
+    "hoofddorp",
+    "nieuw-vennep",
+    "badhoevedorp",
+    "zwanenburg",
+    "schiphol",
+    "rijsenhout",
+    "vijfhuizen",
+    "halfweg",
+    "lijnden",
+    "buitenkaag",
+    # Woonplaatsen in Houten
+    "houten",
+    "schalkwijk",
+    "tull en 't waal",
+    "'t goy",
+    # Woonplaatsen in IJsselstein
+    "ijsselstein",
+    # Woonplaatsen in Landsmeer
+    "landsmeer",
+    "den ilp",
+    "purmerland",
+    # Woonplaatsen in Leusden
+    "leusden",
+    "achterveld",
+    # Woonplaatsen in Lopik
+    "lopik",
+    "benschop",
+    "cabauw",
+    "jaarsveld",
+    "lopikerkapel",
+    "polsbroek",
+    # Woonplaatsen in Montfoort
+    "montfoort",
+    "linschoten",
+    # Woonplaatsen in Nieuwegein
+    "nieuwegein",
+    # Woonplaatsen in Oostzaan
+    "oostzaan",
+    # Woonplaatsen in Ouder-Amstel
+    "duivendrecht",
+    "ouderkerk aan de amstel",
+    "waver",
+    # Woonplaatsen in Oudewater
+    "oudewater",
+    "hekendorp",
+    "papekop",
+    "snelrewaard",
+    # Woonplaatsen in Purmerend
+    "purmerend",
+    "purmerbuurt",
+    "middenbeemster",  # Beemster is samengevoegd met Purmerend
+    # Woonplaatsen in Renswoude
+    "renswoude",
+    # Woonplaatsen in Rhenen
+    "rhenen",
+    "elst",
+    "achterberg",
+    # Woonplaatsen in Soest
+    "soest",
+    "soesterberg",
+    # Woonplaatsen in Stichtse Vecht
+    "maarssen",
+    "breukelen",
+    "loenen aan de vecht",
+    "kockengen",
+    "nieuwer ter aa",
+    "nigtevecht",
+    "vreeland",
+    "tienhoven",
+    # Woonplaatsen in Uithoorn
+    "uithoorn",
+    "de kwakel",
+    # Woonplaatsen in Utrecht
+    "utrecht",
+    "vleuten",
+    "de meern",
+    "haarzuilens",
+    # Woonplaatsen in Utrechtse Heuvelrug
+    "driebergen-rijsenburg",
+    "doorn",
+    "leersum",
+    "amerongen",
+    "maarn",
+    "overberg",
+    # Woonplaatsen in Veenendaal
+    "veenendaal",
+    # Woonplaatsen in Vijfheerenlanden
+    "leerdam",
+    "vianen",
+    "lexmond",
+    "hei- en boeicop",
+    "meerkerk",
+    "ameide",
+    "tienhoven aan de lek",
+    "hagestein",
+    "everdingen",
+    "hoef en haag",
+    # Woonplaatsen in Waterland
+    "monnickendam",
+    "marken",
+    "broek in waterland",
+    "zuiderwoude",
+    "uitdam",
+    "ilpendam",
+    "katwoude",
+    # Woonplaatsen in Wijk bij Duurstede
+    "wijk bij duurstede",
+    "cothen",
+    "langbroek",
+    # Woonplaatsen in Woerden
+    "woerden",
+    "harmelen",
+    "kamerik",
+    "zegveld",
+    # Woonplaatsen in Woudenberg
+    "woudenberg",
+    # Woonplaatsen in Zeist
+    "zeist",
+    "den dolder",
+    "austerlitz",
+    "bosch en duin",
+    "huis ter heide",
+)
