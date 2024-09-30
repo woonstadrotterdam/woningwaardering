90680bb893c50995c0903eda3d53b0c0cebbc4b0
Tomer Gabay <tomergabay001@gmail.com>
2024-09-30 12:16:48 +0200

    opzet onzelfstandige woonruimten

---
+++ woningwaardering/stelsels/onzelfstandige_woonruimten/__init__.py
@@ -0,0 +1,5 @@
+from .oppervlakte_van_vertrekken import OppervlakteVanVertrekken
+
+__all__ = [
+    "OppervlakteVanVertrekken",
+]
---
+++ woningwaardering/stelsels/onzelfstandige_woonruimten/onzelfstandige_woonruimten.py
@@ -0,0 +1,53 @@
+import warnings
+from datetime import date
+
+from loguru import logger
+
+from woningwaardering.stelsels import utils
+from woningwaardering.stelsels.onzelfstandige_woonruimten import (
+    OppervlakteVanVertrekken,
+)
+from woningwaardering.stelsels.stelsel import Stelsel
+from woningwaardering.vera.bvg.generated import (
+    EenhedenEenheid,
+)
+from woningwaardering.vera.referentiedata import (
+    Woningwaarderingstelsel,
+)
+
+
+class OnzelfstandigeWoonruimten(Stelsel):
+    def __init__(self, peildatum: date = date.today()) -> None:
+        super().__init__(
+            stelsel=Woningwaarderingstelsel.onzelfstandige_woonruimten,
+            begindatum=date.fromisoformat("2024-07-01"),
+            einddatum=date.max,
+            peildatum=peildatum,
+            stelselgroepen=[
+                OppervlakteVanVertrekken,
+            ],
+        )
+
+
+if __name__ == "__main__":  # pragma: no cover
+    logger.enable("woningwaardering")
+    warnings.simplefilter("default", UserWarning)
+
+    onzelfstandige_woonruimten = OnzelfstandigeWoonruimten(
+        peildatum=date.fromisoformat("2024-07-01")
+    )
+
+    with open(
+        "tests/data/generiek/input/37101000032.json",
+        "r+",
+    ) as file:
+        eenheid = EenhedenEenheid.model_validate_json(file.read())
+        woningwaardering_resultaat = onzelfstandige_woonruimten.bereken(eenheid)
+        print(
+            woningwaardering_resultaat.model_dump_json(
+                by_alias=True, indent=2, exclude_none=True
+            )
+        )
+        tabel = utils.naar_tabel(woningwaardering_resultaat)
+
+        print(tabel)
