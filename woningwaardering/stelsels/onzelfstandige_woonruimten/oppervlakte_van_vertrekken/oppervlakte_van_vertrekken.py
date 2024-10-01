f7d46aaafd4f272168724e4559db21d12e46924e
Tomer Gabay <tomergabay001@gmail.com>
2024-10-01 13:47:43 +0200

    logica voor oppervlakte van vertrekken onzelfstandig toegevoegd

--- woningwaardering/stelsels/onzelfstandige_woonruimten/oppervlakte_van_vertrekken/oppervlakte_van_vertrekken.py
+++ woningwaardering/stelsels/onzelfstandige_woonruimten/oppervlakte_van_vertrekken/oppervlakte_van_vertrekken.py
@@ -5,6 +5,9 @@ from loguru import logger
 
 from woningwaardering.stelsels import utils
 from woningwaardering.stelsels.stelselgroep import Stelselgroep
+from woningwaardering.stelsels.zelfstandige_woonruimten.oppervlakte_van_vertrekken.oppervlakte_van_vertrekken import (
+    OppervlakteVanVertrekken as ZelfstandigeWoonruimtenOppervlakteVanVertrekken,
+)
 from woningwaardering.vera.bvg.generated import (
     EenhedenEenheid,
     WoningwaarderingResultatenWoningwaardering,
@@ -48,21 +51,41 @@ class OppervlakteVanVertrekken(Stelselgroep):
 
         woningwaardering_groep.woningwaarderingen = []
 
-        woningwaardering_groep.woningwaarderingen.append(
-            WoningwaarderingResultatenWoningwaardering(
-                criterium=WoningwaarderingResultatenWoningwaarderingCriterium(
-                    naam="NotImplemented"
-                )
+        for ruimte in eenheid.ruimten or []:
+            woningwaarderingen = ZelfstandigeWoonruimtenOppervlakteVanVertrekken.genereer_woningwaarderingen(
+                ruimte, self.stelselgroep
             )
-        )
+            gedeelde_ruimte = (
+                ruimte.gedeeld_met_aantal_eenheden is not None
+                and ruimte.gedeeld_met_aantal_eenheden >= 2
+            )
+            for woningwaardering in woningwaarderingen:
+                if gedeelde_ruimte:
+                    woningwaardering.criterium.naam = f"{woningwaardering.criterium.naam} (gedeeld met {ruimte.gedeeld_met_aantal_eenheden})"
+                    woningwaardering.punten = utils.rond_af(
+                        utils.rond_af(woningwaardering.aantal, decimalen=2)
+                        / ruimte.gedeeld_met_aantal_eenheden,
+                        decimalen=2,
+                    )
+                else:
+                    woningwaardering.punten = utils.rond_af(
+                        woningwaardering.aantal, decimalen=2
+                    )
+                woningwaardering_groep.woningwaarderingen.append(woningwaardering)
 
-        punten = utils.rond_af(
-            sum(
-                Decimal(str(woningwaardering.punten))
-                for woningwaardering in woningwaardering_groep.woningwaarderingen or []
-                if woningwaardering.punten is not None
-            ),
-            decimalen=0,
+        punten = utils.rond_af_op_kwart(
+            float(
+                utils.rond_af(
+                    sum(
+                        Decimal(str(woningwaardering.aantal))
+                        for woningwaardering in woningwaardering_groep.woningwaarderingen
+                        or []
+                        if woningwaardering.aantal is not None
+                    ),
+                    decimalen=0,
+                )
+                * Decimal("1")
+            )
         )
 
         woningwaardering_groep.punten = float(punten)
