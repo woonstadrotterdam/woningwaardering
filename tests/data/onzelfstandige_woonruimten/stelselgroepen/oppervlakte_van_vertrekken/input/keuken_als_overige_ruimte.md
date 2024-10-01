00370ffe1270c20c5dc5d5a29f8a05ee81755b73
Tomer Gabay <tomergabay001@gmail.com>
2024-10-01 16:09:42 +0200

    specifieke tests toegevoegd voor oppervlakte van vertrekken onz

---
+++ tests/data/onzelfstandige_woonruimten/stelselgroepen/oppervlakte_van_vertrekken/input/keuken_als_overige_ruimte.md
@@ -0,0 +1,1 @@
+Deze file bevat 1 keuken. Een keuken is altijd een vertrek en deze moet dan ook gewaardeerd worden onder oppervlakte van vertrekken.
---
+++ tests/data/onzelfstandige_woonruimten/stelselgroepen/oppervlakte_van_vertrekken/test_OppervlakteVanVertrekken.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+
+import pytest
+
+from tests.test_utils import assert_output_model, laad_specifiek_input_en_output_model
+from woningwaardering.stelsels.onzelfstandige_woonruimten import (
+    OppervlakteVanVertrekken,
+)
+from woningwaardering.stelsels.utils import normaliseer_ruimte_namen
+from woningwaardering.vera.bvg.generated import (
+    WoningwaarderingResultatenWoningwaarderingGroep,
+    WoningwaarderingResultatenWoningwaarderingResultaat,
+)
+from woningwaardering.vera.referentiedata import Woningwaarderingstelselgroep
+
+# def test_OppervlakteVanVertrekken(
+#     zelfstandige_woonruimten_inputmodel, woningwaardering_resultaat, peildatum
+# ):
+#     oppervlakte_van_vertrekken = OppervlakteVanVertrekken(peildatum=peildatum)
+#     resultaat = oppervlakte_van_vertrekken.bereken(
+#         zelfstandige_woonruimten_inputmodel, woningwaardering_resultaat
+#     )
+#     assert isinstance(resultaat, WoningwaarderingResultatenWoningwaarderingGroep)
+
+
+# def test_OppervlakteVanVertrekken_output(
+#     zelfstandige_woonruimten_input_en_outputmodel, peildatum
+# ):
+#     eenheid_input, eenheid_output = zelfstandige_woonruimten_input_en_outputmodel
+
+#     normaliseer_ruimte_namen(eenheid_input)
+
+#     oppervlakte_van_vertrekken = OppervlakteVanVertrekken(peildatum=peildatum)
+
+#     resultaat = WoningwaarderingResultatenWoningwaarderingResultaat()
+#     resultaat.groepen = [oppervlakte_van_vertrekken.bereken(eenheid_input)]
+
+#     assert_output_model(
+#         resultaat,
+#         eenheid_output,
+#         Woningwaarderingstelselgroep.oppervlakte_van_vertrekken,
+#     )
+
+
+# Get the absolute path to the current file
+current_file_path = Path(__file__).absolute().parent
+
+
+@pytest.fixture(params=[str(p) for p in (current_file_path / "output").rglob("*.json")])
+def specifieke_input_en_output_model(request):
+    output_file_path = request.param
+    return laad_specifiek_input_en_output_model(
+        current_file_path, Path(output_file_path)
+    )
+
+
+def test_OppervlakteVanVertrekken_specifiek_output(
+    specifieke_input_en_output_model, peildatum
+):
+    eenheid_input, eenheid_output = specifieke_input_en_output_model
+    oppervlakte_van_vertrekken = OppervlakteVanVertrekken(peildatum=peildatum)
+
+    resultaat = WoningwaarderingResultatenWoningwaarderingResultaat()
+    resultaat.groepen = [oppervlakte_van_vertrekken.bereken(eenheid_input)]
+
+    assert_output_model(
+        resultaat,
+        eenheid_output,
+        Woningwaarderingstelselgroep.oppervlakte_van_vertrekken,
+    )
