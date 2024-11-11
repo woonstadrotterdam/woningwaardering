322f347cf782132ca3b3939df6bb9d9226ae836c
Tomer Gabay <tomergabay001@gmail.com>
2024-11-11 12:38:44 +0100

    tests geupdatet na refactoring

--- tests/data/onzelfstandige_woonruimten/stelselgroepen/bijzondere_voorzieningen/test_BijzondereVoorzieningenOnz.py
+++ tests/data/onzelfstandige_woonruimten/stelselgroepen/bijzondere_voorzieningen/test_BijzondereVoorzieningenOnz.py
@@ -4,7 +4,7 @@ import pytest
 
 from tests.test_utils import assert_output_model, laad_specifiek_input_en_output_model
 from woningwaardering.stelsels.onzelfstandige_woonruimten import (
-    BijzondereVoorzieningenZorgwoning,
+    BijzondereVoorzieningen,
 )
 from woningwaardering.stelsels.utils import normaliseer_ruimte_namen
 from woningwaardering.vera.bvg.generated import (
@@ -17,10 +17,8 @@ from woningwaardering.vera.referentiedata import Woningwaarderingstelselgroep
 def test_BijzondereVoorzieningenZorgwoning(
     onzelfstandige_woonruimten_inputmodel, woningwaardering_resultaat, peildatum
 ):
-    bijzondere_voorzieningen_zorgwoning = BijzondereVoorzieningenZorgwoning(
-        peildatum=peildatum
-    )
-    resultaat = bijzondere_voorzieningen_zorgwoning.bereken(
+    bijzondere_voorzieningen = BijzondereVoorzieningen(peildatum=peildatum)
+    resultaat = bijzondere_voorzieningen.bereken(
         onzelfstandige_woonruimten_inputmodel, woningwaardering_resultaat
     )
     assert isinstance(resultaat, WoningwaarderingResultatenWoningwaarderingGroep)
@@ -33,17 +31,15 @@ def test_BijzondereVoorzieningenZorgwoning_output(
 
     normaliseer_ruimte_namen(eenheid_input)
 
-    bijzondere_voorzieningen_zorgwoning = BijzondereVoorzieningenZorgwoning(
-        peildatum=peildatum
-    )
+    bijzondere_voorzieningen = BijzondereVoorzieningen(peildatum=peildatum)
 
     resultaat = WoningwaarderingResultatenWoningwaarderingResultaat()
-    resultaat.groepen = [bijzondere_voorzieningen_zorgwoning.bereken(eenheid_input)]
+    resultaat.groepen = [bijzondere_voorzieningen.bereken(eenheid_input)]
 
     assert_output_model(
         resultaat,
         eenheid_output,
-        Woningwaarderingstelselgroep.bijzondere_voorzieningen_zorgwoning,
+        Woningwaarderingstelselgroep.bijzondere_voorzieningen,
     )
 
 
@@ -63,15 +59,13 @@ def test_BijzondereVoorzieningenZorgwoning_specifiek_output(
     specifieke_input_en_output_model, peildatum
 ):
     eenheid_input, eenheid_output = specifieke_input_en_output_model
-    bijzondere_voorzieningen_zorgwoning = BijzondereVoorzieningenZorgwoning(
-        peildatum=peildatum
-    )
+    bijzondere_voorzieningen = BijzondereVoorzieningen(peildatum=peildatum)
 
     resultaat = WoningwaarderingResultatenWoningwaarderingResultaat()
-    resultaat.groepen = [bijzondere_voorzieningen_zorgwoning.bereken(eenheid_input)]
+    resultaat.groepen = [bijzondere_voorzieningen.bereken(eenheid_input)]
 
     assert_output_model(
         resultaat,
         eenheid_output,
-        Woningwaarderingstelselgroep.bijzondere_voorzieningen_zorgwoning,
+        Woningwaarderingstelselgroep.bijzondere_voorzieningen,
     )
