0308a6060881a3fac7057db33e2cacb23fe9dd71
Tomer Gabay <tomergabay001@gmail.com>
2024-11-12 17:16:22 +0100

    Woningwaarderingclass toegevoegd die zelf stelsel kiest

---
+++ woningwaardering/_setup.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import time
+import warnings
+from decimal import BasicContext, setcontext
+from types import TracebackType
+
+from loguru import logger
+
+# Set context for all calculations to avoid rounding errors
+# See https://docs.python.org/3/library/decimal.html#rounding
+setcontext(BasicContext)
+
+default_timezone = "Europe/Amsterdam"
+
+
+def setup_timezone() -> None:
+    timezone = os.environ.get("TZ")
+
+    if timezone is None:
+        logger.info('Geen environment variable "TZ" gevonden')
+        logger.info(f"Tijdzone wordt ingesteld op {default_timezone}")
+        os.environ["TZ"] = default_timezone
+        time.tzset()
+    else:
+        logger.debug(f'Tijdzone ingesteld via environment variable "TZ": {timezone}')
+
+
+def handle_unhandled_exception(
+    exception_type: type[BaseException],
+    exception_value: BaseException,
+    exception_traceback: TracebackType | None,
+) -> None:
+    """
+    Deze functie zorgt ervoor dat onverwachte uitzonderingen gelogged worden
+    en negeert daarbij uitzonderingen die ontstaan zijn door een KeyboardInterrupt.
+    Als de uitzondering van het type `KeyboardInterrupt` is, wordt de uitvoering
+    onderbroken zonder enige verdere actie.
+    Een voorbeeld van een `KeyboardInterrupt` is wanneer een gebruiker op Ctrl+C drukt.
+    """
+    if issubclass(exception_type, KeyboardInterrupt):
+        return
+    logger.error(exception_value)
+    sys.__excepthook__(exception_type, exception_value, exception_traceback)
+
+
+def initialize():
+    logger.disable("woningwaardering")
+    setup_timezone()
+    sys.excepthook = handle_unhandled_exception
+    warnings.simplefilter("error", UserWarning)
