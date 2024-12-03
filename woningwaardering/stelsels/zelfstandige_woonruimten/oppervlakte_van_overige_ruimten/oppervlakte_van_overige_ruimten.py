b74fbab6d2d2666026696eb7979892e866889928
Tomer Gabay <tomergabay001@gmail.com>
2024-12-03 12:58:06 +0100

    comment statement verbeterd in oppervlakte van overige ruimten zelfstandig

--- woningwaardering/stelsels/zelfstandige_woonruimten/oppervlakte_van_overige_ruimten/oppervlakte_van_overige_ruimten.py
+++ woningwaardering/stelsels/zelfstandige_woonruimten/oppervlakte_van_overige_ruimten/oppervlakte_van_overige_ruimten.py
@@ -78,7 +78,7 @@ class OppervlakteVanOverigeRuimten(Stelselgroep):
                 )
                 * Decimal("0.75")
             )
-            # de maximering is altijd in punten en daarom wordt de som de maximering hier van de punten afgetrokken
+            # de maximering is altijd in punten en daarom wordt de som van de punten hier gebruikt om de maximering toe te passsen
             + sum(
                 Decimal(str(woningwaardering.punten))
                 for woningwaardering in woningwaardering_groep.woningwaarderingen or []
