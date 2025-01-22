3c4f2fe2bcfdccd53d1aef392c8e382c45825075
Ben Verhees <ben.verhees@woonstadrotterdam.nl>
2025-01-22 11:04:05 +0100

    ValidationError als warning

--- woningwaardering/vera/bvg/model_uitbreidingen/eenheden_eenheid.py
+++ woningwaardering/vera/bvg/model_uitbreidingen/eenheden_eenheid.py
@@ -27,14 +27,18 @@ class _EenhedenEenheid(BaseModel):
     @classmethod
     def warning_bij_validatiefout(
         cls, value: Any, handler: ValidatorFunctionWrapHandler, info: ValidationInfo
-    ):
+    ) -> Any:  # pragma: no cover
+        validation_error: ValidationError | None = None
+
         try:
             return handler(value)
         except ValidationError as err:
-            errors: list[str] = []
+            validation_error = err
 
+        if validation_error:
+            errors: list[str] = []
             # Loop door alle fouten in de ValidationError
-            for error in err.errors():
+            for error in validation_error.errors():
                 # Haal het pad naar het veld op waar de fout optrad
                 locs = tuple(error.get("loc", []))
                 # Maak een leesbaar pad door veldnamen met punten te verbinden
@@ -47,14 +51,12 @@ class _EenhedenEenheid(BaseModel):
                 )
                 errors.append(readable_error)
 
-            # Geef alle fouten als één waarschuwing
             warnings.warn(" ".join(errors), UserWarning)
 
             # Verwijder het veld met de fout
-            for error in err.errors():
+            for error in validation_error.errors():
                 locs = tuple(error.get("loc", []))
                 parent_object = reduce(getitem, locs[:-1], value)
-
                 if isinstance(parent_object, dict):
                     del parent_object[locs[-1]]
                 else:
