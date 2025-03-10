036f9c164fcaf2199d66d9ac93335701b8363a83
Ben Verhees <ben.verhees@woonstadrotterdam.nl>
2025-03-10 12:24:11 +0100

    Remove print

--- docs/implementatietoelichtingen/voorbeelden/zelfstandige_woonruimten/sanitair/extra_voorzieningen.py
+++ docs/implementatietoelichtingen/voorbeelden/zelfstandige_woonruimten/sanitair/extra_voorzieningen.py
@@ -43,11 +43,6 @@ def get_eenheid():
 
     eenheid.woningwaarderingstelsel = Woningwaarderingstelsel.zelfstandige_woonruimten
 
-    print(
-        eenheid.model_dump_json(
-            indent=2, exclude_none=True, by_alias=True, exclude_defaults=True
-        )
-    )
     return eenheid
 
 
